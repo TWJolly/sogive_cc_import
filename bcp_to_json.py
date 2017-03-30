442d4c50dae9ef96e260bc6d9f8391c3b4189bc9
Toby <tobiasjolly@gmail.com>
2017-03-31 00:10:54 +0100

    set bcp_to_json to read test_csvs

--- bcp_to_json.py
+++ bcp_to_json.py
@@ -1,18 +1,18 @@
 from unzip_cc_data import import_zip, cc_files
 import pandas as pd
+import os
 
-#Change this to the location of downloaded data
-path_to_cc_zip = "RegPlusExtract_March_2017.zip"
+dir_name = os.path.dirname(os.path.abspath(__file__))
+csv_file_dir = 'test_csvs'
+os.chdir(os.path.join(dir_name, csv_file_dir))
 
+path_to_cc_zip = "RegPlusExtract_March_2017.zip" #Change this to the location of downloaded data
+#import_zip(path_to_cc_zip) #Uncomment to perform the extract from bcp files
 
-basic_charity_info = 'extract_charity'
-classification_refernece = 'extract_class_ref'
-
-#import_zip(path_to_cc_zip)
 all_data_tables = {}
-
 for file in cc_files:
     file_name = file + ".csv"
     print('Reading file: '+file_name)
     all_data_tables[file] = pd.read_csv(file_name, skiprows=[1], error_bad_lines = False, encoding='utf-8')
+    print(all_data_tables)
 
